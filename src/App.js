f997019972553f3a2134ecfd5ee517219c9345ca
Getachew Mulat <gech4me@gmail.com>
2019-11-13 09:36:09 +0300

    setting up the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-class App extends React.Component{
+class App extends Component{
   render() {
     return (
         <div>
